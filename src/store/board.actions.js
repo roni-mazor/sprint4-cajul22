e11625ca36687bb01b90575ed2a5944d5227c6d3
roni-mazor <ro6596@gmail.com>
2022-09-15 18:41:16 +0300

    merging css conflicts

--- src/store/board.actions.js
+++ src/store/board.actions.js
@@ -20,6 +20,7 @@ export function loadBoards() {
         }
 
     }
+}
 
 
 export function updateIsStarred(board) {
--- src/store/board.reducer.js
+++ src/store/board.reducer.js
@@ -9,7 +9,7 @@ export function boardReducer(state = initialState, action) {
 
     switch (action.type) {
         case 'SET_BOARDS':
-            return { ...state, boards: action.board }
+            return { ...state, boards: action.boards }
         case 'SET_BOARD':
             return { ...state, board: { ...action.board } }
         case 'SET_STARRED':
