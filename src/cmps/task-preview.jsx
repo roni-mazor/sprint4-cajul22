288e7a07aa90153f688338b66e27d3d413dde840
roni-mazor <ro6596@gmail.com>
2022-09-16 12:09:50 +0300

    desplaying labels in preview

--- src/cmps/task-preview.jsx
+++ src/cmps/task-preview.jsx
@@ -1,12 +1,19 @@
+import { useSelector } from "react-redux"
 import { Link } from "react-router-dom"
 
 export const TaskPreview = ({ task, boardId, groupId }) => {
-
-    // console.log(task)
+    const labels = useSelector(state => state.boardModule.board.labels)
+    console.log(task.labelIds)
+    console.log(labels)
     return (
         <Link to={`/board/${boardId}/${groupId}/${task.id}`} className="task-preview">
             <header className="task-header"></header>
             <section className="labels-container">
+                {task.labelIds.map((id) => {
+                    const label = labels.find(l => l.id === id)
+                    console.log(label)
+                    return <div style={{ backgroundColor: label.color }} ></div>
+                })}
             </section>
             <p>{task.title}</p>
             <section className="task-badges"></section>
