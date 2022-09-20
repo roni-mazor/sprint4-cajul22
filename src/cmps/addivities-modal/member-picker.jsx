86cf77e740bc28524e062bccb5417ca0bf1af734
Omer Rafaeli <69318815+OmerRafaeli@users.noreply.github.com>
2022-09-20 09:02:33 +0300

    final members update and css work

--- src/cmps/addivities-modal/member-picker.jsx
+++ src/cmps/addivities-modal/member-picker.jsx
@@ -1,23 +1,46 @@
 import { VscChromeClose } from 'react-icons/vsc'
-import { useState } from 'react'
-// import { useDispatch } from 'react-redux'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import { useSelector } from 'react-redux'
 import { MemberPreview } from '../member-preview'
 import { utilService } from '../../services/util.service'
 
-export const MemberPicker = ({ onSaveTask, task, toggleModal }) => {
+export const MemberPicker = ({ onSaveTask, task, toggleModal, isJoinedChange }) => {
     const board = useSelector(state => state.boardModule.board)
 
-    const [filterBy, setFilterBy] = useState('')
-    let members = useSelector(state => state.boardModule.board.members)
-    // members = members.filter(member => member.fullname.includes(filterBy))
+    const dispatch = useDispatch()
+    const [txt, setTxt] = useState('')
+    const loggedIn = useSelector(state => state.userModule.user)
+    const members = useSelector(state => state.boardModule.board.members)
+
+    useEffect(() => {
+
+    }, [txt])
 
     const onAddMemberToTask = (memberId) => {
-        const member = board.members.find(member => member._id === memberId)
-        // console.log('task:', task)       
-        task.members = [...task.members, member]
+        let currMember = task.members.find(member => (member._id === memberId))
+        if(currMember){
+            // if(loggedIn._id === currMember._id) {isJoinedChange()}
+            const selectedMembers = task.members.filter(member => member._id !== memberId)
+            task.members = [...selectedMembers]
+            onSaveTask(task)
+            return
+        } 
+
+        currMember = board.members.find(member => member._id === memberId)
+        task.members = [...task.members, currMember]
         
         onSaveTask(task)
+    }
+
+    const onHandleChange = ({ target: { value } }) => {
+        setTxt(value)
+    }
+
+    const getFilteredUsers = () => {
+        const regex = new RegExp(txt, 'i')
+        const currMembers = members.filter(member => regex.test(member.fullname))
+        return currMembers
     }
 
     return (
@@ -33,12 +56,13 @@ export const MemberPicker = ({ onSaveTask, task, toggleModal }) => {
             <hr />
 
             <input className="label-title-input"
-                type="text" placeholder='Search members'
-                onChange={({ target: { value } }) => { setFilterBy(value) }}
-                value={filterBy} />
+                type="text" 
+                placeholder='Search members'
+                onChange={onHandleChange}
+                value={txt} />
             <p>Board members</p>
             <ul className="labels-container">
-                {board.members.map(member => (
+                {getFilteredUsers().map(member => (
                     <li key={utilService.makeId(5)} >
                         <span className='member-container flex align-center' onClick={() => onAddMemberToTask(member._id)}>
                             <MemberPreview member={member} />
